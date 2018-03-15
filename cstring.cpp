0811d4f3c71457866d199afba4447724a422cf40
Arkhane <arkhane@okstate.edu>
2018-03-14 20:18:55 -0500

    lab1_week_9
    lab1_week_9

---
+++ cstring.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char name[6] = "Bobby";
+	char last[8] = "Rockers";
+	char copy[8];
+
+	strcpy(copy, last, 4)
+
+	cout << "Firstname: " << name << "Lastname: " << copy << endl;
+
+	
+}
